18156e9bea1db6c488cb1f21a60c310f3ea1a3f5
Hari <anarahari@gmail.com>
2017-09-27 23:44:45 +0800

    first draft mocha tests

---
+++ tests/test_flightdata.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+describe('FlightData', function() {
+  describe('#get_airlines()', function() {
+    it('should return a list of airlines in the world', function() {
+    	throw new Error("not implemented");
+    });
+  });
+  describe('#get_airports()', function() {
+    it('should return a list of airports in the world', function() {
+    	throw new Error("not implemented");
+    });
+  });
+  describe('#get_countries()', function() {
+    it('should return a list of countries in the world', function() {
+    	throw new Error("not implemented");
+    });
+  });
+  describe('#get_history_by_tail_number()', function() {
+    it('should return the flight history of the aircraft', function() {
+    	throw new Error("not implemented");
+    });
+  });
+  describe('#get_history_by_flight_number()', function() {
+    it('should return the flight history of that flight number', function() {
+    	throw new Error("not implemented");
+    });
+  });
+});
