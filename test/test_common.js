097d482a4c0bcb0f87ce5a9e5bab6410e40d2c46
Hari <anarahari@gmail.com>
2017-10-21 11:59:56 +0800

    added basic tests for common

---
+++ test/test_common.js
@@ -0,0 +1,22 @@
+var assert = require('assert')
+var common = require('../lib/common.js')
+
+
+describe('Common tests', function(){
+	describe('#get_page_or_none()',function(){
+		xit('should return none for invalid page',function(){
+
+		})
+		xit('should retrun the page content for a valid page address',function(){
+
+		})
+	})
+	describe('#get_raw_data_json()',function(){
+		xit('should return non if the data from url is not json',function(){
+
+		})
+		xit('should return json data for a valid url', function () {
+			
+		})
+	})
+})
--- test/test_flightdata.js
+++ test/test_flightdata.js
@@ -1,45 +1,45 @@
-var assert = require('assert');
-var flightdata = require('../lib/flightdata.js');
+var assert = require('assert')
+var flightdata = require('../lib/flightdata.js')
 
 describe('FlightData - basic tests', function() {
   describe('#get_airlines()', function() {
     it('should return a list of airlines in the world', function() {
     	flightdata.get_airlines().length>0;
-    });
-  });
+    })
+  })
   describe('#get_airports()', function() {
     it('should return a list of airports in the world', function() {
     	flightdata.get_airports().length>0;
-    });
-  });
+    })
+  })
   describe('#get_countries()', function() {
     it('should return a list of countries in the world', function() {
     	flightdata.get_countries().length>0;
-    });
-  });
+    })
+  })
   describe('#get_history_by_tail_number()', function() {
     it('should return the flight history of the aircraft',function(){
       flightdata.get_history_by_tail_number('9V-SMA').length>0;
-    });
-  });
+    })
+  })
   describe('#get_history_by_flight_number()', function() {
     it('should return the flight history of that flight number',function(){
       flightdata.get_history_by_tail_number('AI-101').length>0;
-    });
-  });
+    })
+  })
   describe('#get_info_by_tail_number()', function() {
     it('should return the flight info of that tail number',function(){
       flightdata.get_info_by_tail_number('AI-101').length>0;
-    });
-  });
+    })
+  })
   describe('#get_flights()', function() {
     it('should return the flights for that airline', function() {
       flightdata.get_flights('air india').length>0;
-    });
-  });
+    })
+  })
   describe('#get_fleet()', function() {
     it('should return the fleet details for that airline', function() {
       flightdata.get_fleet('air india').length>0;
-    });
-  });
-});
+    })
+  })
+})
