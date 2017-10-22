d47cb6d57a28ddc4e42b2d12d3a2d179496124f9
Hari <anarahari@gmail.com>
2017-10-22 23:29:59 +0800

    attempting to test some requests code

--- test/test_common.js
+++ test/test_common.js
@@ -4,11 +4,11 @@ var common = require('../lib/common.js')
 
 describe('Common tests', function(){
 	describe('#get_page_or_none()',function(){
-		xit('should return none for invalid page',function(){
-
+		it('should return none for invalid page',function(){
+			assert.equal(common.get_page_or_none('invalid_url'),null)
 		})
 		xit('should retrun the page content for a valid page address',function(){
-
+			assert.notEqual(common.get_page_or_none('www.google.com'),null)
 		})
 	})
 	describe('#get_raw_data_json()',function(){
