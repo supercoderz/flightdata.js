6a88825526f80410fb82976113b838ac273fa4af
Hari <anarahari@gmail.com>
2017-10-30 14:55:07 +0800

    more tests

--- test/test_common_fr24.js
+++ test/test_common_fr24.js
@@ -19,6 +19,8 @@ describe('FR24 common tests',function(){
 		it('should return the data for a flight route from the url in json format',function(){
 			fr24.get_data(fr24.FLT_BASE('AI101'),function(res){
 				should.exist(res)
+				should.exist(res.result.response.data)
+				assert.isAtMost(res.result.response.data.length,100)
 			},function(err){
 				assert.fail(0, 1, 'unexpected error in response')
 			})
@@ -26,6 +28,26 @@ describe('FR24 common tests',function(){
 		it('should return the data for a aircraft reg from the url in json format',function(){
 			fr24.get_data(fr24.REG_BASE('9V-SMA'),function(res){
 				should.exist(res)
+				should.exist(res.result.response.data)
+				assert.isAtMost(res.result.response.data.length,100)
+			},function(err){
+				assert.fail(0, 1, 'unexpected error in response')
+			})
+		})
+		it('should return the data for a aircraft reg and limits from the url in json format',function(){
+			fr24.get_data(fr24.REG_BASE('9V-SMA',page=1,limit=10),function(res){
+				should.exist(res)
+				should.exist(res.result.response.data)
+				assert.isAtMost(res.result.response.data.length,10)
+			},function(err){
+				assert.fail(0, 1, 'unexpected error in response')
+			})
+		})
+		it('should return the data for a aircraft reg,page and limits from the url in json format',function(){
+			fr24.get_data(fr24.REG_BASE('9V-SMA',page=10,limit=10),function(res){
+				should.exist(res)
+				should.exist(res.result.response.data)
+				assert.isAtMost(res.result.response.data.length,10)
 			},function(err){
 				assert.fail(0, 1, 'unexpected error in response')
 			})
